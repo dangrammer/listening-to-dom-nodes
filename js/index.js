358fb5c37e2f6344507e499747d1f606e74f895e
dangrammer <danieljromans@gmail.com>
2020-02-07 18:54:27 -0500

    examples complete

--- js/index.js
+++ js/index.js
@@ -1,2 +1,38 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+const main = document.getElementById('main')
+const input = document.querySelector('input')
+
+main.addEventListener('click', function(event) {
+  alert('I was clicked!')
+})
+
+// input.addEventListener('keydown', function(e) {
+  //   console.log(e.key);
+  // })
+  
+input.addEventListener('keydown', function(e) {
+  if (e.key === "g") {
+    return e.preventDefault()
+  } else {
+    console.log(e.key)
+  }
+})
+  
+function bubble(e) {
+  e.stopPropagation()
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled')
+}
+  
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+
+let divs = document.querySelectorAll('div')
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble)
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', capture, true);
+}
+
